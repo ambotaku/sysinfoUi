97cb14a4a6ff8201d3899c4bd9d271067dc8acc2
kzerbe <klaus@zerbe-net.org>
2018-04-22 15:35:22 +0200

    added InterfaceStatus display

--- src/app/network-interfaces.component.ts
+++ src/app/network-interfaces.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AppState, InterfaceStatus, NetInterface} from "./models";
+import {AppState, NetInterface} from "./models";
 import {Store} from "@ngrx/store";
 import {fetchNetInterfaces} from "./actions";
 import {Observable} from "rxjs/Observable";
--- src/app/selectors.ts
+++ src/app/selectors.ts
@@ -1,6 +1,5 @@
 import {AppState} from "./models";
 
-
 export const getInterfaces = (state: AppState) => state.sysInfo.interfaces;
 export const getMemory = (state: AppState) => state.sysInfo.memory;
 export const getError = (state: AppState) => state.sysInfo.error;
