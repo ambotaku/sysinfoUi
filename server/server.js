e54e075336b83f85fa16763a9a55059691f9765c
kzerbe <klaus@zerbe-net.org>
2018-04-22 09:52:19 +0200

    moved server code

---
+++ server/server.js
@@ -0,0 +1,41 @@
+let sysInfo = require('systeminformation');
+let express = require('express');
+let app = express();
+let port = process.env.PORT || 3030;
+
+let router = express.Router();
+app.use('/api', router);
+
+router.get('/cpu', (req, res) => {
+  sysInfo.cpu(data => {
+    res.json(data);
+  });
+});
+
+router.get('/system', (req, res) => {
+  sysInfo.system(data => {
+    res.json(data);
+  });
+});
+
+router.get('/mem', (req, res) => {
+  sysInfo.mem(data => {
+    res.json(data);
+  });
+});
+
+router.get('/network-interfaces', (req, res) => {
+  sysInfo.networkInterfaces(data => {
+    res.json(data);
+  });
+});
+
+router.get('/processes', (req, res) => {
+  sysInfo.processes(data => {
+    res.json(data);
+  });
+});
+
+app.disable('etag');
+app.listen(port);
+console.log('Listening on port ' + port);
